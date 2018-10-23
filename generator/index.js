6516f7d31a5bbf8e94355695f2694370a78ab084
Rain Wang <rainwzp@gmail.com>
2018-10-23 13:52:55 +0800

    add mock

--- generator/index.js
+++ generator/index.js
@@ -3,10 +3,24 @@ module.exports = api => {
     dependencies: {
       'vue-router': '^3.0.1',
       'vuex': '^3.0.1',
-      'vue-router-layout': '^0.1.2'
+      'vue-router-layout': '^0.1.2',
+      "nodemon": "^1.18.4",
+      "json-server": "^0.14.0",
+      "mockjs": "^1.0.1-beta3"
     },
     devDependencies: {
       'vue-macula-auto-routing': '^0.3.5'
+    },
+    scripts: {
+      "serve": "vue-cli-service serve",
+      "build-dev": "vue-cli-service build --mode production.dev",
+      "build-test": "vue-cli-service build --mode production.test",
+      "build-staging": "vue-cli-service build --mode production.staging",
+      "build": "vue-cli-service build",
+      "lint": "vue-cli-service lint",
+      "mock": "nodemon --watch mock mocks/server.js",
+      "test:e2e": "vue-cli-service test:e2e --mode development",
+      "test:unit": "vue-cli-service test:unit"
     }
   })
 
